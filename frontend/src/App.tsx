c2794f51efa676cfc133bf4abcdf8fa7354ad6ad
Thiago Alves Ferreira <thiagoalvesferreira.taf@gmail.com>
2023-08-08 16:26:26 -0300

    Aula 01/08/2023

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import TopicCard from "./components/TopicCard"
+import { Divider } from "@mui/material";
+
 function App() {
 
   return (
-    <div id="App">
-      
+    <div id='App'>
+      <TopicCard/>
+      <Divider/>
+      <TopicCard/>
+      <Divider/>
+      <TopicCard/>
+      <Divider/>
     </div>
   )
 }
