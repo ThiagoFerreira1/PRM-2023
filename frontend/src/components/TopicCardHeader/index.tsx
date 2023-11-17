7824728834ffd3d8d09b5475922e04b74264342c
Thiago Alves Ferreira <thiagoalvesferreira.taf@gmail.com>
2023-11-17 19:11:45 -0300

    Aula 14/11

--- frontend/src/components/TopicCardHeader/index.tsx
+++ frontend/src/components/TopicCardHeader/index.tsx
@@ -1,26 +1,39 @@
-import { Avatar, Typography } from "@mui/material";
+import {Avatar, Typography} from '@mui/material';
 
 import './style.css';
+import { IUser } from '../../@types/Index';
+import { FormattedDate, IntlProvider } from 'react-intl';
 
-function TopicCardHeader() {
-  return (
+type TopicCardHeaderProps = {
+    createdAt: Date | undefined;
+    owner: IUser | undefined;
+}
 
-    <div id="topic-card-header">
-      <Avatar alt="Fulano de tal"/>
+function TopicCardHeader({
+    createdAt,
+    owner
+}: TopicCardHeaderProps) {
+    return (
+        <div id="topic-card-header">
+            <Avatar alt={owner?.fullname} />
 
-      <div className="card-header-text">
-        <Typography variant="h6">
-          Fulano de Tal
-        </Typography>
+            <div className="card-header-text">
+                <Typography variant="h6">
+                    {owner?.fullname}
+                </Typography>
 
-        <Typography variant="caption">
-          Criado há 8 horas
-        </Typography>
-      </div>
+                <Typography variant="caption">
+                    <IntlProvider locale='pt-BR'>
+                        Criado em <FormattedDate value={createdAt} day='2-digit'
+                                        month='2-digit' year='numeric' />
+                    </IntlProvider>
+                    
+                </Typography>
+            </div>
+            
+        </div>
 
-    </div>
-
-  )
+    )
 }
 
 export default TopicCardHeader;
--- frontend/src/components/TopicList/index.tsx
+++ frontend/src/components/TopicList/index.tsx
@@ -12,7 +12,7 @@ function TopicList({ items }: TopicListProps) {
       display="flex"
       flexDirection="column"
       gap={3}
-      style={{ maxWidth: "64rem" }}
+      style={{marginTop: '2rem', maxWidth: "64rem" }}
     >
       {items.map((item: any, index: number) => (
         <Box display="flex" flexDirection="column" gap={3}
